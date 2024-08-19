1494534bce022c706d367d881b16e85ca7a5b18b
sakshisahu07 <sakshisahu072@gmail.com>
2024-08-18 22:56:54 -0700

    Atc from product display

--- src/pages/ProductDisplay.jsx
+++ src/pages/ProductDisplay.jsx
@@ -1,7 +1,10 @@
 import { useLocation } from "react-router-dom";
 import { useState,useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addtoCart } from "../CartSlice";
 
 const ProductDisplay=()=>{
+    const dispatch=useDispatch()
     const [mypro,setMypro]=useState({
         id:"",
         name:"",
@@ -21,6 +24,10 @@ const ProductDisplay=()=>{
             }
         )
     },[])
+
+    const DataCart=(pid,nm,img,desc,price)=>{
+        dispatch(addtoCart({id:pid,name:nm,image:img,description:desc,qnty:1, price:price}))
+    }
     return(
         <>
         <h1>Our Product Display here!!!</h1>
@@ -32,7 +39,7 @@ const ProductDisplay=()=>{
                 <h2>Product name:{mypro.name}</h2>
                 <h4 style={{color:"blue"}}>Description:{mypro.description}</h4>
                 <h2 style={{color:"red"}}>Price:{mypro.price}</h2>
-                <button>Add to cart</button>
+                <button onClick={()=>{DataCart(mypro.id, mypro.name, mypro.images, mypro.description, mypro.price)}}>ADD to Cart</button>
             </div>
 
         </div>
